59fd15c098580c4722460948d8079e0da0828fd7
Alvarobyui <u701860138@byui.edu>
2023-06-22 23:59:15 -0500

    New_isometric_images added

--- src/content/services.jsx
+++ src/content/services.jsx
@@ -1,6 +1,9 @@
 import colorWheel from "../assets/services/color-wheel.png";
-import photography from "../assets/services/photography.png";
+import teamWork from "../assets/services/Team_work_Isometric.png";
 import UI from "../assets/services/ui.png";
+import designer from "../assets/services/Designer_Isometric.png";
+import bilingual from "../assets/services/Bilingual.png"
+
 export const services = [
   {
     icon: colorWheel,
@@ -8,12 +11,12 @@ export const services = [
     description: "Analyze and apply principles of design, typography, color, and photography to create various design projects",
   },
   {
-    icon: photography,
+    icon: designer,
     title: "Web frontend development",
     description: "Advanced knoledge of HTML, CSS, SASS, JavaScript, React, and Next.",
   },
   {
-    icon: UI,
+    icon: teamWork,
     title: "Teamwork expert",
     description: " Demonstrate the traits of an influencial team member and advanced knoledge of SCRUM, Git, and GitHub",
   },
@@ -23,7 +26,7 @@ export const services = [
     description: "Write operational code with the Object-Oriented Programming paradigm",
   },
   {
-    icon: UI,
+    icon: bilingual,
     title: "Bilingual proficiency",
     description: "Native Spanish speaker with advanced proficiency in English (B2/C1) along effective communicative skills in both languages.",
   },
