56569c8781550781cf7d57b808bd2ce31dd31be8
Alvarobyui <u701860138@byui.edu>
2023-07-18 21:04:41 -0500

    form automatization implemented

--- src/components/Contacts.jsx
+++ src/components/Contacts.jsx
@@ -1,14 +1,38 @@
 import styles from "./Contacts.module.css";
-
+import { useState } from "react";
 import sendIcon from "../assets/actions/send.png";
 
 export const Contacts = () => {
+  const[result, setResult] = useState("");
+  
+  const onSubmit = async (event) => {
+    event.preventDefault();
+    setResult("Sending...");
+    const formData = new FormData(event.target);
+    
+    formData.append("access_key", "ce3069e4-d8d4-4039-b966-562795683b98");
+
+    const res = await fetch("https://api.web3forms.com/submit", {
+      method: "POST",
+      body: formData
+    }).then((res) => res.json());
+
+    if (res.success) {
+      console.log("Success", res);
+      setResult("Sent! Thank you for contacting me");
+    } else {
+      console.log("Error", res);
+      setResult(res.message);
+    }
+  };
+
   return (
     <form
       className={styles.wrapper}
       target="_blank"
-      action="https://formsubmit.co/your@email.com"
-      method="POST"
+      /* action="https://formsubmit.co/your@email.com"
+      method="POST" */
+      onSubmit={onSubmit}
     >
       <div className={styles.inputs}>
         <input
@@ -38,6 +62,7 @@ export const Contacts = () => {
           <img className={styles.icon} src={sendIcon} alt="" />
         </button>
       </div>
+      <p className={styles.result}>{result}</p>
     </form>
   );
 };
