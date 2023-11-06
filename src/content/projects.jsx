a62ea04f4c530640215874cf39982df967e96eb5
Your Name <you@example.com>
2023-11-06 10:41:10 -0500

    change: orden de proyectos

--- src/content/projects.jsx
+++ src/content/projects.jsx
@@ -7,28 +7,10 @@ import UniversityImg from "../assets/projects/university.png"
  */ 
 export const projects = [
   {
-    title: "Student Information System with roles - (PHP)",
-    description: "A web application that manages permissions, registers students and teachers, and is divided by roles. It also allows for inputting academic grades, editing data, uploading scores, and displaying course information for students.",
-    image: "/assets/projects/university.png",
-    link: "https://universidadwebapp.000webhostapp.com/"
-  },
-  {
-    title: "School API - (Laravel)",
-    description: "An API implemented in Laravel to record the attendance of students in a course, encompassing students, teachers, and courses. ",
-    image: "/assets/projects/university.png",
-    link: "https://github.com/Alvarobyui/small_school_API"
-  },
-  {
     title: "Forecast App - (Next JS)",
     description: "A responsive, adaptive, forecast application implemented in Next.",
     image: "/assets/projects/weather-app.png",
     link: "https://forecast-weather-web-app.vercel.app/"
-  },
-  {
-    title: "Tic Tac Toe - (React JS)",
-    description: "A Tic Tac Toe game implemented in React.",
-    image: "/assets/projects/Tic_tac_toe_generated-vecteezy.com.jpg",
-    link: "https://alvarobyui.github.io/Tic-Tac-Toe/"
   },
   {
     title: "Temple Inn & Suits website",
@@ -37,15 +19,33 @@ export const projects = [
     link: "https://alvarobyui.github.io/Temple-Inn-Suite-Website/"
   },
   {
-    title: "Online marketplace for accommodations",
-    description: "A fully responsive site similar to Airbnb displaying information from a JSON file or API that has a funtional search box. This has been implemented in React",
-    image: "/assets/projects/Real Estate Agent_Monochromatic.png",
-    link: "https://alvarobyui.github.io/Mini-project/#"
+    title: "Student Information System with roles - (PHP)",
+    description: "A web application that manages permissions, registers students and teachers, and is divided by roles. It also allows for inputting academic grades, editing data, uploading scores, and displaying course information for students.",
+    image: "/assets/projects/university.png",
+    link: "https://universidadwebapp.000webhostapp.com/"
+  },
+  {
+    title: "Tic Tac Toe - (React JS)",
+    description: "A Tic Tac Toe game implemented in React.",
+    image: "/assets/projects/Tic_tac_toe_generated-vecteezy.com.jpg",
+    link: "https://alvarobyui.github.io/Tic-Tac-Toe/"
   },
   {
     title: "Tip Calculator",
     description: "A demo in pure JavaScript that calculates how much a client should pay for their bill after adding a tip.",
     image: "/assets/projects/Data_Arranging_Monochromatic.png",
     link: "https://alvarobyui.github.io/Tip-Calculator/"
+  },
+  {
+    title: "Online marketplace for accommodations",
+    description: "A fully responsive site similar to Airbnb displaying information from a JSON file or API that has a funtional search box. This has been implemented in React",
+    image: "/assets/projects/Real Estate Agent_Monochromatic.png",
+    link: "https://alvarobyui.github.io/Mini-project/#"
+  },
+  {
+    title: "School API - (Laravel)",
+    description: "An API implemented in Laravel to record the attendance of students in a course, encompassing students, teachers, and courses. ",
+    image: "/assets/projects/university.png",
+    link: "https://github.com/Alvarobyui/small_school_API"
   },
 ];
